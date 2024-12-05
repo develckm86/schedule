679acd6a5e789828ece0d2eaa92ce49ba276d40e
develckm86 <develckm86@gmail.com>
2024-12-05 23:02:20 +0900

    달력에 날짜 형식 및 날짜 추가하기

--- app.js
+++ app.js
@@ -1,24 +1,11 @@
-//container.innerText="???";//defer
-
-//project
-//1. 로그인 유저를 먼저 불러오기 (상품 리스트,공휴일)
-//2. 그 유저와 관련된 데이터 불러오기(장바구니,스케줄)
-
-const loadData=async function(){
-    let resArr=await Promise.all([
-        fetch("./loginUser.json"),
-        fetch("./products.json")
-    ]);
-    let objArr=await Promise.all([
-        resArr[0].json(),
-        resArr[1].json()
-    ]);
-    console.log(objArr);
-    const loginUser=objArr[0];
-    const products=objArr[1];
-    //baskets?user_id=develckm; => deveckmBaskets.json
-    let res3=await fetch(`./${loginUser["user_id"]}Baskets.json`);
-    let baskets=await res3.json();
-    console.log(baskets); 
+const now=new Date(); //현재날짜
+let meonthSchedules;
+const loadSchedule=async ()=>{
+    //2024_12_schedule.json (현재 날짜 불러오기)
+    let fileName=`./data/${now.getFullYear()}_${now.getMonth()+1}_schedule.json`;
+    let resArr=Promise.all([
+        fetch(fileName),
+        fetch("./data/loginUser.json")
+    ]);     
 }
-loadData();
+loadSchedule();
